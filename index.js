b8e2dc6cebddc2803624e107f60cc9f705330864
Dominic Vermeulen-Smith <contact@bedevious.co.uk>
2024-10-30 16:09:29 +0000

    update toggl api to v9

--- index.js
+++ index.js
@@ -1,42 +1,31 @@
-const dotenv = require( 'dotenv' );
+const dotenv = require('dotenv');
 dotenv.config();
 
-const date = require( 'date-and-time' );
-const axios = require( 'axios' );
+const date = require('date-and-time');
+const axios = require('axios');
 
-var TogglClient = require( 'toggl-api' );
-var toggl = new TogglClient({ apiToken: process.env.TOGGLEAPIKEY });
-
-var lookupPlackerCard = async ( description ) => {
-
+var lookupPlackerCard = async (description) => {
   try {
-
-    let response = await axios.get( 'https://placker.com/public/api/card?title=' + description , {
+    let response = await axios.get('https://placker.com/public/api/card?title=' + description, {
       headers: {
         'X-API-KEY': process.env.PLACKERAPIKEY
       },
     });
 
-    if( response.data.length === 0 ) {
+    if (response.data.length === 0) {
       return {};
     }
 
     return response.data[0];
-
-  }
-
-  catch( err ) {
-    console.log( err.response.statusText );
+  } catch (err) {
+    console.log(err.response.statusText);
     return {};
   }
+};
 
-}
-
-var updatePlackerCardEffort = async ( id , duration , externalFields ) => {
-
+var updatePlackerCardEffort = async (id, duration, externalFields) => {
   try {
-
-    let response = await axios.patch( 'https://placker.com/public/api/card/' + id , {
+    let response = await axios.patch('https://placker.com/public/api/card/' + id, {
       actualEffort: duration,
       externalFields: externalFields,
     }, {
@@ -46,79 +35,85 @@ var updatePlackerCardEffort = async ( id , duration , externalFields ) => {
     });
 
     return true;
-
-  }
-
-  catch( err ) {
-    console.log( err.response.statusText );
+  } catch (err) {
+    console.log(err.response.statusText);
     return false;
   }
+};
 
-}
+var getTimeEntries = async (start, end) => {
+  try {
+    let response = await axios.get('https://api.track.toggl.com/api/v9/me/time_entries', {
+      auth: {
+        username: process.env.TOGGLEAPIKEY,
+        password: 'api_token'
+      },
+      params: {
+        start_date: start.toISOString(),
+        end_date: end.toISOString()
+      }
+    });
+    return response.data;
+  } catch (err) {
+    console.log('Error fetching time entries:', err.response?.statusText || err.message);
+    return [];
+  }
+};
 
-exports.run = ( event , context ) => {
-
+exports.run = async (event, context) => {
   const now = new Date();
-  const minus15 = date.addHours( now , -6 );
+  const minus15 = date.addHours(now, -6);
 
-  const start = date.parse( date.format( minus15 , 'YYYY/MM/DD HH:mm:ss' ) , 'YYYY/MM/DD HH:mm:ss' );
-  const end = date.parse( date.format( now , 'YYYY/MM/DD HH:mm:ss' ) , 'YYYY/MM/DD HH:mm:ss' );
+  const start = date.parse(date.format(minus15, 'YYYY/MM/DD HH:mm:ss'), 'YYYY/MM/DD HH:mm:ss');
+  const end = date.parse(date.format(now, 'YYYY/MM/DD HH:mm:ss'), 'YYYY/MM/DD HH:mm:ss');
 
-  toggl.getTimeEntries( start , end , async ( err , timeEntries ) => {
+  let timeEntries = await getTimeEntries(start, end);
 
-    if( err ) {
-      console.log( 'Error looking up time entries' );
-      return;
+  for (let timeEntry of timeEntries) {
+    if (typeof(timeEntry.stop) === 'undefined') {
+      continue;
     }
 
-    for( let timeEntry of timeEntries ) {
+    let actualEffort = 0;
+    let cardTitle = timeEntry.description;
+    let externalFields = {};
+    let relatedCard = await lookupPlackerCard(cardTitle);
 
-      if( typeof( timeEntry.stop ) === 'undefined' ) {
-        continue;
-      }
+    console.log( relatedCard );
 
-      let actualEffort = 0;
-      let cardTitle = timeEntry.description;
-      let externalFields = {};
-      let relatedCard = await lookupPlackerCard( cardTitle );
-
-      if( typeof( relatedCard.id ) === 'undefined' ) {
-        continue;
-      }
-
-      if( typeof( relatedCard.effort.actual ) !== 'undefined' ) {
-        actualEffort = relatedCard.effort.actual * 60;
-      }
-
-      if( typeof( relatedCard.externalFields ) !== 'undefined' ) {
-        externalFields = relatedCard.externalFields;
-      }
-
-      if( typeof( externalFields.toggleIds ) !== 'undefined' ) {
-        externalFields.toggleIds.value = externalFields.toggleIds.value.split( ',' );
-      } else {
-        externalFields.toggleIds = {
-          value: [],
-        };
-      }
-
-      if( externalFields.toggleIds.value.indexOf( timeEntry.id.toString()) !== -1 ) {
-        continue;
-      }
-
-      externalFields.toggleIds.value.push( timeEntry.id );
-      externalFields.toggleIds.value = externalFields.toggleIds.value.join( ',' );
-
-      sendExternalFields = [{
-        name: 'toggleIds',
-        value: externalFields.toggleIds.value,
-      }]
-
-      let updatedEffort = actualEffort + timeEntry.duration;
-      await updatePlackerCardEffort( relatedCard.id , updatedEffort , sendExternalFields );
-
+    if (typeof(relatedCard.id) === 'undefined') {
+      continue;
     }
 
-  });
+    if (typeof(relatedCard.effort.actual) !== 'undefined') {
+      actualEffort = relatedCard.effort.actual * 60;
+    }
 
+    if (typeof(relatedCard.externalFields) !== 'undefined') {
+      externalFields = relatedCard.externalFields;
+    }
+
+    if (typeof(externalFields.toggleIds) !== 'undefined') {
+      externalFields.toggleIds.value = externalFields.toggleIds.value.split(',');
+    } else {
+      externalFields.toggleIds = {
+        value: [],
+      };
+    }
+
+    if (externalFields.toggleIds.value.indexOf(timeEntry.id.toString()) !== -1) {
+      continue;
+    }
+
+    externalFields.toggleIds.value.push(timeEntry.id);
+    externalFields.toggleIds.value = externalFields.toggleIds.value.join(',');
+
+    let sendExternalFields = [{
+      name: 'toggleIds',
+      value: externalFields.toggleIds.value,
+    }];
+
+    let updatedEffort = actualEffort + timeEntry.duration;
+    await updatePlackerCardEffort(relatedCard.id, updatedEffort, sendExternalFields);
+  }
 };
